142ceb86c35973e96846d99ae1a588cd7d4904fc
pasanmanula <pasanmanula@gmail.com>
2021-04-21 00:12:29 -0500

    added vertex shader and fragment shader

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -2,6 +2,45 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id,1,&src,nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS,&result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id,GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id,length,&length,message);
+        std::cout << "Failed to compile" << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER,vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -29,17 +68,46 @@ int main(void)
 
     ///   
 
+    //Vertex butter- Copy positions array to vRAM of the GPU AND select that buffer. (Remember this is a state machine)
     float positions[6] = {
         -0.5f,-0.5f,
          0.0f, 0.5f,
          0.5f, -0.5f
     };
     unsigned int buffer;
-    glGenBuffers(1,&buffer);
-    glBindBuffer(GL_ARRAY_BUFFER,buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float),positions,GL_STATIC_DRAW);
+    glGenBuffers(1,&buffer); //One buffer and pointer to a unsigned int--> Generating a buffer and giving us an ID to refer later.
+    glBindBuffer(GL_ARRAY_BUFFER,buffer); //Selecting(glbind**) the above buffer
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float),positions,GL_STATIC_DRAW); //Put the data into the buffer
 
+    //Vertex Attributes - Telling our Layout (Here positions)
+    glEnableVertexAttribArray(0); //Enable the vertex attribute
+    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,sizeof(float)*2,0); //Telling the layout
 
+    //Shader - block of code that run on the GPU
+    //Vertex shader - This shader will call for every vertex (3 times in this case). Primary purpose :- Where the vertex should place in the screen.
+    //Fragment/Pixel shader - Runs once for each pixel that needs to get rasterized(drawn on the screen). Primary purpose :- Decide which color that pixel supposed to be.
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location =0) in vec4 position;\n"
+        "\n"
+        "void main()"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location =0) out vec4 color;\n"
+        "\n"
+        "void main()"
+        "{\n"
+        "   color = vec4(1.0,0.0,0.0,1.0);\n"
+        "}\n";
+
+    unsigned int shader = CreateShader(vertexShader,fragmentShader);
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) 
@@ -61,7 +129,7 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
-
+    glDeleteProgram(shader);
     glfwTerminate();
     return 0;
 }
